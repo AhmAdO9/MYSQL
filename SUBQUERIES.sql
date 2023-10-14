b191a3765ca97d291be783a93bf229ea41d87865
faheem <adahm7114@gmail.com>
2023-10-14 22:33:55 +0530

    _

--- SUBQUERIES.sql
+++ SUBQUERIES.sql
@@ -158,21 +158,30 @@ USE sql_invoicing;
 -- SUBQUERIES IN THE FROM CLAUSE
 
 
-SELECT 
-		name,
-		(
-		SELECT SUM(invoice_total)
-        FROM invoices
-        WHERE client_id = c.client_id
-        ) AS total_sales,
-        (
-		SELECT AVG(invoice_total)
-        FROM invoices
-        ) AS average,
-        (SELECT total_sales)-(SELECT average) AS difference
-        
-FROM clients c  
-HAVING total_sales IS NOT NULL  
+-- SELECT *
+-- FROM  
+-- (
+-- 		SELECT 
+--         
+-- 			client_id,
+-- 			name,
+-- 			(
+-- 			SELECT SUM(invoice_total)
+-- 			FROM invoices
+-- 			WHERE client_id = c.client_id
+-- 			) AS total_sales,
+-- 			(
+-- 			SELECT AVG(invoice_total)
+-- 			FROM invoices
+-- 			) AS average,
+-- 			(SELECT total_sales)-(SELECT average) AS difference
+--             
+-- 		FROM clients c  
+-- 		HAVING total_sales IS NOT NULL  
+
+-- ) AS sales_summary
+-- WHERE client_id = 3
+
 
 
 
