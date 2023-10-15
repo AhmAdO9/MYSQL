32f0998f9cb4d5d42bdcfbd1c2e435e9b82c48dd
faheem <adahm7114@gmail.com>
2023-10-15 18:57:29 +0530

    _

---
+++ MySQL_Functions.sql
@@ -0,0 +1,98 @@
+-- NUMERIC FUNCTIONS
+
+-- ROUND 
+-- SELECT ROUND(5.73,1)
+
+-- TRUNCATE
+-- SELECT TRUNCATE(5.73636, 2)
+
+-- CEILING`
+-- SELECT CEILING(5.1)
+
+-- FLOOR
+-- SELECT FLOOR(2.3)
+
+-- ABSOLUTE
+-- SELECT ABS(-2.5)
+
+-- RANDOM
+-- SELECT RAND()
+
+-- STRING FUNCTIONS
+
+-- LENGTH
+-- SELECT LENGTH('sky')
+
+-- UPPER AND LOWER
+
+-- SELECT UPPER('sky')
+-- SELECT LOWER('SKY')
+
+-- TRIM
+-- SELECT TRIM('   SKY    ')
+
+-- LEFT 
+-- SELECT LEFT('kindergarten',4)
+
+-- RIGHT
+-- SELECT RIGHT('kindergarten', 2)
+
+-- SUBSTRING
+-- SELECT SUBSTRING('kindergarten', 3)
+
+-- LOCATE
+-- SELECT LOCATE('garten', 'kindergarten')
+
+-- REPLACE
+-- SELECT REPLACE('kindergarten', 'garten', 'garden')
+ 
+ 
+ -- CONCAT
+ -- SELECT CONCAT('first', ' last')
+ 
+ 
+ -- DATE_FUNCTIONS
+ 
+ -- SELECT NOW(), CURDATE(), CURTIME()
+ 
+ -- YEAR
+-- SELECT YEAR(NOW()) 
+ 
+-- MONTH
+-- SELECT YEAR(NOW())
+
+-- DAY
+-- SELECT YEAR(NOW()) 
+ 
+-- HOUR
+-- SELECT YEAR(NOW())
+
+-- MINUTE
+-- SELECT YEAR(NOW())
+
+-- SECOND
+-- SELECT YEAR(NOW())
+
+-- DAYNAME
+-- SELECT DAYNAME(NOW())
+
+-- SELECT EXTRACT(DAY FROM NOW())
+ 
+ 
+ -- FORMATTING DATES AND TIME
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
